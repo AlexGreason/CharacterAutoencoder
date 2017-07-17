3cf68a14394b30f8291a0503aa67de9461521c92
AlexanderGreason <quantum935@yahoo.com>
2017-07-17 00:00:08 -0500

    Update to keras 2 api

--- OmniglotAutoencoderGrid.py
+++ OmniglotAutoencoderGrid.py
@@ -2,7 +2,7 @@ import time
 
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, Reshape, Flatten, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Reshape, Flatten, UpSampling2D
 from keras.models import Model
 
 from helperfunctions import genvec
@@ -14,10 +14,10 @@ latent_dim = 16
 intermediate_dim = 256
 
 x = Input(batch_shape=original_shape)
-a = Convolution2D(128, 5, 5, border_mode='same', activation='relu')(x)
+a = Conv2D(128, (5, 5), padding='same', activation='relu')(x)
 b = MaxPooling2D(pool_size=(4, 4))(a)
-c = Convolution2D(128, 3, 3, border_mode='same', activation='relu')(b)
-d = Convolution2D(16, 3, 3, border_mode='same', activation='relu')(c)
+c = Conv2D(128, (3,3), padding='same', activation='relu')(b)
+d = Conv2D(16, (3,3), padding='same', activation='relu')(c)
 d_reshaped = Flatten()(d)
 h = Dense(intermediate_dim, activation='relu')(d_reshaped)
 z_mean = Dense(latent_dim)(h)
@@ -26,11 +26,11 @@ z_mean = Dense(latent_dim)(h)
 decoder_h = Dense(intermediate_dim, activation='relu')
 i = Dense(8 * 24 * 24, activation='relu')
 j = Reshape((8, 24, 24))
-k = Convolution2D(128, 3, 3, border_mode='same', activation='relu')
+k = Conv2D(128, (3,3), padding='same', activation='relu')
 l = UpSampling2D((4, 4))
-m = Convolution2D(128, 3, 3, border_mode='same', activation='relu')
-n = Convolution2D(128, 3, 3, border_mode='same', activation='relu')
-decoder_mean = Convolution2D(1, 3, 3, border_mode='same', activation='sigmoid')
+m = Conv2D(128, (3,3), padding='same', activation='relu')
+n = Conv2D(128, (3,3), padding='same', activation='relu')
+decoder_mean = Conv2D(1, (3,3), padding='same', activation='sigmoid')
 
 h_decoded = decoder_h(z_mean)
 i_decoded = i(h_decoded)
@@ -44,15 +44,18 @@ x_decoded_mean = decoder_mean(n_decoded)
 vae = Model(x, x_decoded_mean)
 vae.compile(optimizer='rmsprop', loss="binary_crossentropy")
 
-computer = "desktop"
+load = False
 
-if computer == "laptop":
-    x_train = np.load("/home/exa/Documents/PythonData/images_all_processed.npy")
+if load:
+    computer = "desktop"
 
-elif computer == "desktop":
-    x_train = np.load("D:\\conlangstuff\\images_all_processed.npy")
+    if computer == "laptop":
+        x_train = np.load("/home/exa/Documents/PythonData/images_all_processed.npy")
 
-x_train = x_train.reshape((x_train.shape[0], 1, sidelen, sidelen))
+    elif computer == "desktop":
+        x_train = np.load("D:\\conlangstuff\\images_all_processed.npy")
+
+    x_train = x_train.reshape((x_train.shape[0], 1, sidelen, sidelen))
 vae.load_weights("omniglot_16_1.sav")
 
 # build a model to project inputs on the latent space
@@ -101,4 +104,4 @@ def showgrid(n, lowerbound, upperbound, type):
     plt.show()
 
 if __name__ == "__main__":
-    showgrid(20, -5, 5, "existing")
+    showgrid(20, -5, 5, "randaxes")
